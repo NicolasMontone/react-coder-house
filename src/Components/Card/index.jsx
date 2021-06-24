78ac08f72974bfc04c55955a2e56950f21e09b12
Nicolas Montone <montonenicolas01@gmail.com>
2021-06-24 11:52:55 -0300

    agregué el onAdd y el initial

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,17 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ItemCount } from "../ItemCount/"
 import "./styles.css"
 export const Card = ({ productos }) => {
-
+  const onAdd = (num) => {
+    alert(`agregaste ${num} productos al carrito`)
+  }
   return (
     <div className="card" style={{ width: '18rem' }}>
       <img src={productos.img} className="card-img-top" alt="Remera hombre" />
       <div className="card-body">
         <h5 className="card-title">{productos.title}</h5>
         <p className="card-text">{productos.precio}</p>
-        <ItemCount stock={productos.stock} />
-        <button className="btn btn-primary">
-          Comprar ahora
-        </button>
+        <ItemCount stock={productos.stock} onAdd={onAdd} initial={1} />
       </div>
     </div>
   );
--- src/Components/ItemCount/index.jsx
+++ src/Components/ItemCount/index.jsx
@@ -2,17 +2,21 @@ import './styles.css'
 import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
 
-export const ItemCount = ({ stock }) => {
-    const [counter, setCounter] = useState(1)
+export const ItemCount = ({ stock, onAdd, initial }) => {
+
+    const [counter, setCounter] = useState(initial)
+
     const restar = () => {
         if (counter > 1)
             setCounter(counter - 1)
     }
+
     const sumar = () => {
         if (counter < stock) {
             setCounter(counter + 1)
         }
     }
+
     const manualChange = (e) => {
         let value = e.target.value
         if (value > stock) {
@@ -21,10 +25,14 @@ export const ItemCount = ({ stock }) => {
             setCounter(value)
         }
     }
+
     return (
         <div>
             <Button variant="outline-primary" onClick={restar}>-</Button>
             <input type="number" value={counter} onChange={manualChange} />
             <Button variant="outline-primary" onClick={sumar}>+</Button>
+            <button className="btn btn-primary" onClick={() => { onAdd(counter) }}>
+                Comprar ahora
+            </button>
         </div>)
 }
